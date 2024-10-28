c61d2807993a1762cab1aa26290d27c934b0d9f4
jfontan93 <167186600+jfontan93@users.noreply.github.com>
2024-10-28 18:52:02 +0000

    correccion

--- src/app.js
+++ src/app.js
@@ -1,27 +1,38 @@
-/* eslint-disable
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};*/
-
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let domainResult = [".es", ".net", ".io", ".org"];
 
-for (let i = 0; i < pronoun.length; i++) {
-for (let j = 0; j < adj.length; j++) {
-for (let k = 0; k < noun.length; k++) {
-for (let i = 0; i < domainResult.length; l++){
-    domainResult.push(pronoun[i] + adj[j] + noun[k] + domainResult[l]);
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domainResult = [".es", ".net", ".io", ".org"];
+
+  let generatedDomains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domainResult.length; l++) {
+          generatedDomains.push(
+            pronoun[i] + adj[j] + noun[k] + domainResult[l]
+          );
+        }
+      }
+    }
   }
-}
-}
-}
-console.log(domainResult);
+
+  const domainList = document.getElementById("domain-list");
+  generatedDomains.forEach(domain => {
+    const div = document.createElement("div");
+    div.textContent = domain;
+    domainList.appendChild(div);
+  });
+};
